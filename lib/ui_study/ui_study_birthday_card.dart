446823172ff640b2b2786c46df72993df4d99d00
shin-SeungHun <sng7387@gmail.com>
2023-12-20 20:38:26 +0900

    과제 생일축하 카드 만들기

---
+++ lib/ui_study/ui_study_birthday_card.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Ui Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const UiStudy(),
+    );
+  }
+}
+
+class UiStudy extends StatelessWidget {
+  const UiStudy({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Happy',
+              style: TextStyle(
+                fontSize: 80,
+              ),
+            ),
+            Text(
+              'Birthday',
+              style: TextStyle(
+                fontSize: 80,
+              ),
+            ),
+            Text(
+              'Sam!',
+              style: TextStyle(
+                fontSize: 80,
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  'From Emma',
+                  style: TextStyle(fontSize: 30),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
