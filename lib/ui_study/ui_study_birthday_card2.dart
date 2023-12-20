cd27b8f2aa87af59e17286550300cf234d99f64f
shin-SeungHun <sng7387@gmail.com>
2023-12-20 21:20:12 +0900

    과제 생일축하 카드 만들기 2

--- lib/ui_study/ui_study_birthday_card2.dart
+++ lib/ui_study/ui_study_birthday_card2.dart
@@ -33,7 +33,7 @@ class BirthdayCardApp extends StatelessWidget {
             'https://img.freepik.com/free-vector/blur-pink-blue-abstract-gradient-background-vector_53876-174836.jpg?size=626&ext=jpg&ga=GA1.1.1222169770.1702425600&semt=ais',
             fit: BoxFit.cover,
           ),
-          Padding(
+          const Padding(
             padding: EdgeInsets.all(20.0),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
