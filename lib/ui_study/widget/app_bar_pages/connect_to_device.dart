d85a7ca49f28a454dd550d839d47c42a85639bb5
shin-SeungHun <sng7387@gmail.com>
2023-12-21 22:06:53 +0900

    앱바 아이콘 페이지 생성

---
+++ lib/ui_study/widget/app_bar_pages/connect_to_device.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ConnectToDevice extends StatefulWidget {
+  const ConnectToDevice({super.key});
+
+  @override
+  State<ConnectToDevice> createState() => _ConnectToDeviceState();
+}
+
+class _ConnectToDeviceState extends State<ConnectToDevice> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
