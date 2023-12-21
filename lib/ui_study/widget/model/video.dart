3a83c9eae2eb88d01ea5a7d356f9a9fb4e00d6b9
shin-SeungHun <sng7387@gmail.com>
2023-12-22 02:34:59 +0900

    video 모델 수정

--- lib/ui_study/widget/model/video.dart
+++ lib/ui_study/widget/model/video.dart
@@ -1,40 +1,42 @@
 class Video {
-  String _title;
-  String _subTitle;
-  String _thumbnailUrl;
-  String _upLoaderUrl;
+  String title;
+  String subTitle;
+  String thumbnailUrl;
+  String upLoaderUrl;
 
 //<editor-fold desc="Data Methods">
   Video({
-    required String title,
-    required String subTitle,
-    required String thumbnailUrl,
-    required String upLoaderUrl,
-  })  : _title = title,
-        _subTitle = subTitle,
-        _thumbnailUrl = thumbnailUrl,
-        _upLoaderUrl = upLoaderUrl;
+    required this.title,
+    required this.subTitle,
+    required this.thumbnailUrl,
+    required this.upLoaderUrl,
+  });
 
   @override
   bool operator ==(Object other) =>
       identical(this, other) ||
       (other is Video &&
           runtimeType == other.runtimeType &&
-          _title == other._title &&
-          _subTitle == other._subTitle &&
-          _thumbnailUrl == other._thumbnailUrl &&
-          _upLoaderUrl == other._upLoaderUrl);
+          title == other.title &&
+          subTitle == other.subTitle &&
+          thumbnailUrl == other.thumbnailUrl &&
+          upLoaderUrl == other.upLoaderUrl);
 
   @override
   int get hashCode =>
-      _title.hashCode ^
-      _subTitle.hashCode ^
-      _thumbnailUrl.hashCode ^
-      _upLoaderUrl.hashCode;
+      title.hashCode ^
+      subTitle.hashCode ^
+      thumbnailUrl.hashCode ^
+      upLoaderUrl.hashCode;
 
   @override
   String toString() {
-    return 'Video{ _title: $_title, _subTitle: $_subTitle, _thumbnailUrl: $_thumbnailUrl, _upLoaderUrl: $_upLoaderUrl,}';
+    return 'Video{' +
+        ' title: $title,' +
+        ' subTitle: $subTitle,' +
+        ' thumbnailUrl: $thumbnailUrl,' +
+        ' upLoaderUrl: $upLoaderUrl,' +
+        '}';
   }
 
   Video copyWith({
@@ -44,28 +46,28 @@ class Video {
     String? upLoaderUrl,
   }) {
     return Video(
-      title: title ?? _title,
-      subTitle: subTitle ?? _subTitle,
-      thumbnailUrl: thumbnailUrl ?? _thumbnailUrl,
-      upLoaderUrl: upLoaderUrl ?? _upLoaderUrl,
+      title: title ?? this.title,
+      subTitle: subTitle ?? this.subTitle,
+      thumbnailUrl: thumbnailUrl ?? this.thumbnailUrl,
+      upLoaderUrl: upLoaderUrl ?? this.upLoaderUrl,
     );
   }
 
   Map<String, dynamic> toMap() {
     return {
-      '_title': _title,
-      '_subTitle': _subTitle,
-      '_thumbnailUrl': _thumbnailUrl,
-      '_upLoaderUrl': _upLoaderUrl,
+      'title': this.title,
+      'subTitle': this.subTitle,
+      'thumbnailUrl': this.thumbnailUrl,
+      'upLoaderUrl': this.upLoaderUrl,
     };
   }
 
   factory Video.fromMap(Map<String, dynamic> map) {
     return Video(
-      title: map['_title'] as String,
-      subTitle: map['_subTitle'] as String,
-      thumbnailUrl: map['_thumbnailUrl'] as String,
-      upLoaderUrl: map['_upLoaderUrl'] as String,
+      title: map['title'] as String,
+      subTitle: map['subTitle'] as String,
+      thumbnailUrl: map['thumbnailUrl'] as String,
+      upLoaderUrl: map['upLoaderUrl'] as String,
     );
   }
 
