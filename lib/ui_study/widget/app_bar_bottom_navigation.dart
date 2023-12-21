a4971f2cb1c052f4225e423fb24995cf82d73b66
shin-SeungHun <sng7387@gmail.com>
2023-12-21 22:07:21 +0900

    파일 이름 변경

--- lib/ui_study/widget/app_bar_bottom_navigation.dart
+++ lib/ui_study/widget/app_bar_bottom_navigation.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_beginner_class/ui_study/widget/pages/add_content.dart';
-import 'package:flutter_beginner_class/ui_study/widget/pages/home.dart';
-import 'package:flutter_beginner_class/ui_study/widget/pages/shorts.dart';
-import 'package:flutter_beginner_class/ui_study/widget/pages/subscribe.dart';
-import 'package:flutter_beginner_class/ui_study/widget/pages/video_library.dart';
+
+import 'bottom_pages/add_content.dart';
+import 'bottom_pages/home.dart';
+import 'bottom_pages/shorts.dart';
+import 'bottom_pages/subscribe.dart';
+import 'bottom_pages/video_library.dart';
 
 class AppBarBottomNavigation extends StatefulWidget {
   const AppBarBottomNavigation({super.key});
@@ -32,18 +33,39 @@ class _AppBarBottomNavigationState extends State<AppBarBottomNavigation> {
         leading: const FlutterLogo(),
         title: const Text('F tube'),
         elevation: 0,
-        actions: const [
+        actions: [
           Padding(
             padding: EdgeInsets.all(8.0),
-            child: Icon(Icons.tap_and_play),
+            child: IconButton(
+              icon: Icon(Icons.tap_and_play),
+              onPressed: () {
+                // TODO: 'tap_and_play' 아이콘을 눌렀을 때 수행할 동작 추가
+                print('Tap and play icon pressed');
+              },
+            ),
           ),
           Padding(
             padding: EdgeInsets.all(8.0),
-            child: Icon(Icons.notification_add),
+            child: IconButton(
+              icon: Icon(Icons.notification_add),
+              onPressed: () {
+                // TODO: 'notification_add' 아이콘을 눌렀을 때 수행할 동작 추가
+                print('Notification add icon pressed');
+              },
+            ),
           ),
           Padding(
             padding: EdgeInsets.all(8.0),
-            child: Icon(Icons.search),
+            child: IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {
+                // TODO: 'search' 아이콘을 눌렀을 때 수행할 동작 추가
+                // Navigator.push(
+                //   context,
+                //   MaterialPageRoute(builder: (context) => SearchScreen()),
+                // );
+              },
+            ),
           ),
           Padding(
             padding: EdgeInsets.all(8.0),
