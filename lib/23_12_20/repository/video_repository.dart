4a42ff35c02ec5c1672538a98d8b6130e620035f
shin-SeungHun <sng7387@gmail.com>
2023-12-22 02:33:30 +0900

    비디오 데이터 저장소

---
+++ lib/23_12_20/repository/video_repository.dart
@@ -0,0 +1,69 @@
+import '../../ui_study/widget/model/video.dart';
+
+class VideoRepository {
+  List<Video> videoList = [
+    Video(
+        title: '솔직히 장충동 왕족발 맛 없죠?ㅋㅋㅋ',
+        subTitle: '피지컬갤러리 • 조회수 22만회 • 7시간 전',
+        thumbnailUrl:
+            'https://i.ytimg.com/an_webp/GIsGxL43fAY/mqdefault_6s.webp?du=3000&sqp=COSnkawG&rs=AOn4CLBIYOdA0SHThfHaTNzKz-7H6jwfdA',
+        upLoaderUrl:
+            'https://yt3.googleusercontent.com/ytc/AIf8zZTOUA3fJ3Cva_tG2ffitTJF8CYdyQ2RfUPW9Tw5Cg=s176-c-k-c0x00ffffff-no-rj'),
+    Video(
+        title: '[#유퀴즈온더블럭] 팀에 남기 위해 중국에서 제시한 245억 연봉 거절한 페이커⁉ 롤⭐컵 최초로 4회 우승 기록한 월드클래스 페이커의 영앤리치 모먼트🏆 | #갓구운클립',
+        subTitle: 'tvN D ENT • 조회수 2.3만회 • 3시간 전',
+        thumbnailUrl:
+            'https://i.ytimg.com/an_webp/FJrJ2I6gVFM/mqdefault_6s.webp?du=3000&sqp=CPTKkawG&rs=AOn4CLARWU_VdAPtsD8ueWDWPOj_6v_uoQ',
+        upLoaderUrl:
+            'https://yt3.googleusercontent.com/8io7suwe23NeVF2rKY8rEUC_Q-e_ZyJYGX7Z1oiY5eb6v6E_1BBpiOsF6Gc19CNYMscgGKY=s176-c-k-c0x00ffffff-no-rj'),
+    Video(
+        title: '솔직히 장충동 왕족발 맛 없죠?ㅋㅋㅋ',
+        subTitle: '피지컬갤러리 • 조회수 22만회 • 7시간 전',
+        thumbnailUrl:
+            'https://i.ytimg.com/an_webp/GIsGxL43fAY/mqdefault_6s.webp?du=3000&sqp=COSnkawG&rs=AOn4CLBIYOdA0SHThfHaTNzKz-7H6jwfdA',
+        upLoaderUrl:
+            'https://yt3.googleusercontent.com/ytc/AIf8zZTOUA3fJ3Cva_tG2ffitTJF8CYdyQ2RfUPW9Tw5Cg=s176-c-k-c0x00ffffff-no-rj'),
+    Video(
+        title: '솔직히 장충동 왕족발 맛 없죠?ㅋㅋㅋ',
+        subTitle: '피지컬갤러리 • 조회수 22만회 • 7시간 전',
+        thumbnailUrl:
+            'https://i.ytimg.com/an_webp/GIsGxL43fAY/mqdefault_6s.webp?du=3000&sqp=COSnkawG&rs=AOn4CLBIYOdA0SHThfHaTNzKz-7H6jwfdA',
+        upLoaderUrl:
+            'https://yt3.googleusercontent.com/ytc/AIf8zZTOUA3fJ3Cva_tG2ffitTJF8CYdyQ2RfUPW9Tw5Cg=s176-c-k-c0x00ffffff-no-rj'),
+    Video(
+        title: '솔직히 장충동 왕족발 맛 없죠?ㅋㅋㅋ',
+        subTitle: '피지컬갤러리 • 조회수 22만회 • 7시간 전',
+        thumbnailUrl:
+            'https://i.ytimg.com/an_webp/GIsGxL43fAY/mqdefault_6s.webp?du=3000&sqp=COSnkawG&rs=AOn4CLBIYOdA0SHThfHaTNzKz-7H6jwfdA',
+        upLoaderUrl:
+            'https://yt3.googleusercontent.com/ytc/AIf8zZTOUA3fJ3Cva_tG2ffitTJF8CYdyQ2RfUPW9Tw5Cg=s176-c-k-c0x00ffffff-no-rj'),
+    Video(
+        title: '솔직히 장충동 왕족발 맛 없죠?ㅋㅋㅋ',
+        subTitle: '피지컬갤러리 • 조회수 22만회 • 7시간 전',
+        thumbnailUrl:
+            'https://i.ytimg.com/an_webp/GIsGxL43fAY/mqdefault_6s.webp?du=3000&sqp=COSnkawG&rs=AOn4CLBIYOdA0SHThfHaTNzKz-7H6jwfdA',
+        upLoaderUrl:
+            'https://yt3.googleusercontent.com/ytc/AIf8zZTOUA3fJ3Cva_tG2ffitTJF8CYdyQ2RfUPW9Tw5Cg=s176-c-k-c0x00ffffff-no-rj'),
+    Video(
+        title: '솔직히 장충동 왕족발 맛 없죠?ㅋㅋㅋ',
+        subTitle: '피지컬갤러리 • 조회수 22만회 • 7시간 전',
+        thumbnailUrl:
+            'https://i.ytimg.com/an_webp/GIsGxL43fAY/mqdefault_6s.webp?du=3000&sqp=COSnkawG&rs=AOn4CLBIYOdA0SHThfHaTNzKz-7H6jwfdA',
+        upLoaderUrl:
+            'https://yt3.googleusercontent.com/ytc/AIf8zZTOUA3fJ3Cva_tG2ffitTJF8CYdyQ2RfUPW9Tw5Cg=s176-c-k-c0x00ffffff-no-rj'),
+    Video(
+        title: '솔직히 장충동 왕족발 맛 없죠?ㅋㅋㅋ',
+        subTitle: '피지컬갤러리 • 조회수 22만회 • 7시간 전',
+        thumbnailUrl:
+            'https://i.ytimg.com/an_webp/GIsGxL43fAY/mqdefault_6s.webp?du=3000&sqp=COSnkawG&rs=AOn4CLBIYOdA0SHThfHaTNzKz-7H6jwfdA',
+        upLoaderUrl:
+            'https://yt3.googleusercontent.com/ytc/AIf8zZTOUA3fJ3Cva_tG2ffitTJF8CYdyQ2RfUPW9Tw5Cg=s176-c-k-c0x00ffffff-no-rj'),
+    Video(
+        title: '솔직히 장충동 왕족발 맛 없죠?ㅋㅋㅋ',
+        subTitle: '피지컬갤러리 • 조회수 22만회 • 7시간 전',
+        thumbnailUrl:
+            'https://i.ytimg.com/an_webp/GIsGxL43fAY/mqdefault_6s.webp?du=3000&sqp=COSnkawG&rs=AOn4CLBIYOdA0SHThfHaTNzKz-7H6jwfdA',
+        upLoaderUrl:
+            'https://yt3.googleusercontent.com/ytc/AIf8zZTOUA3fJ3Cva_tG2ffitTJF8CYdyQ2RfUPW9Tw5Cg=s176-c-k-c0x00ffffff-no-rj'),
+  ];
+}
